52517fe52fd3a65d1d289e7a6fb8966bb010156d
sattamalmuwallad <sattamalmuwallad@gmail.com>
2025-02-22 22:36:32 +0000

    String lib functions

--- Week2/Strings.c
+++ Week2/Strings.c
@@ -4,5 +4,16 @@ int main(){
     char string[] = "Hello";
     printf("Length (strlen): %lu\n", strlen(string));  // Without null terminator
     printf("Size (sizeof): %lu\n", sizeof(string));    // With null terminator
+    
+
+    // strcpy()
+    char cpy[10]; // has to be with enough size
+    strcpy(cpy, string);
+    printf("Copied string: %s \n", cpy); // common pitfall the output was Hello% with the null terminator
+    //what we should do is 
+    char sfcpy[6];
+    strncpy(sfcpy, string, sizeof(sfcpy) - 1); // safe copy
+    sfcpy[sizeof(sfcpy) - 1] = '\0'; // null terminator
+    printf("Safe copied string is: %s\n", sfcpy);
     return 0;
 }
